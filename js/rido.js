5c80e7afdc2323baf6a6178e166cf52ece41a82a
YuriyCher <y0979441322@ukr.net>
2023-01-06 14:24:48 +0200

    куц

---
+++ js/rido.js
@@ -0,0 +1,16 @@
+// <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>JavaScript is fun!</title>
+  </head>
+
+  <body>
+    let quantity = prompt(`введіть кількість товара`);
+    quantity = Number(quantity);
+    console.log(quantity);
+
+    
+    <script src="js/script.js"></script>
+  </body>
+</html>
