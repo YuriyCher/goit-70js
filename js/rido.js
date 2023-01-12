59c96ed9c1cd265661fb64f51c47169cfb5c2223
YuriyCher <y0979441322@ukr.net>
2023-01-12 22:35:37 +0200

    Update rido.js

--- js/rido.js
+++ js/rido.js
@@ -1,16 +1,18 @@
-// <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>JavaScript is fun!</title>
-  </head>
-
-  <body>
-    let quantity = prompt(`введіть кількість товара`);
-    quantity = Number(quantity);
-    console.log(quantity);
-
-    
-    <script src="js/script.js"></script>
-  </body>
+<head>
+    <meta charset="UTF-8">
+    <title>JS Sandbox</title>
+    <style>
+        body {
+          
+        }
+    </style> 
+</head>
+<body>
+    <div></div>
+    <script src="script.js" type="module"></script>
+    <script src="lesson-01.js" type="module"></script>
+    <script src="lesson-02.js" type="module"></script>
+</body>
 </html>
